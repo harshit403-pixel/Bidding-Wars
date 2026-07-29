5893391df9ebee6102d2d6fd82f2c8ee106f953f
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-07-30 00:44:44 +0530

    feat(middlewares): add various middleware for error handling, authentication, and request validation

---
+++ server/src/shared/middlewares/index.middleware.ts
@@ -0,0 +1,27 @@
+// Importing modules
+import express, { Express } from "express";
+import compression from "compression";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import env from "../config/env.config.js";
+
+// function to apply middlewares to the app
+function applyMiddlewares(app: Express) {
+
+    // applying middlewares
+    app.use(compression());
+
+    app.use(cors());
+
+    app.use(helmet());
+
+    app.use(cookieParser());
+
+    app.use(express.json({ limit: "100kb" }));
+
+    app.use(express.urlencoded({ extended: true, limit: "100kb" }));
+
+}
+
+export default applyMiddlewares;
